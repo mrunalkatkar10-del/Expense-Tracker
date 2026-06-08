2ed3a2a218be11482d7381a947eaa6abcd6fb185
mrunalkatkar10-del <mrunalkatkar10@gmail.com>
2026-06-08 23:39:36 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "expense-tracker-v1";
+const FILES = ["/", "/index.html", "/manifest.json"];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
